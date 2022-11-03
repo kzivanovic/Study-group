1ee499a278ad5fd3fdad503a80792bb84c985be4
Katarina <katarinazivanovic@BEG-Katarina-Zivanovic.local>
2022-11-03 11:43:19 +0100

    Format changes

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,26 +1,27 @@
-describe('Test cases for W&Co', () => {
-  it('Go to work.co', () => {
-    cy.visit('https://www.work.co')
+describe("Test cases for W&Co", () => {
+  it("Go to work.co", () => {
+    cy.visit("https://www.work.co");
+  });
 
-    })
+  it("Check headline", () => {
+    cy.get('[data-test-id="header-title-text"]').should(
+      "have.text",
+      "We design and ship digital products that transform companies."
+    );
+  });
+  it("Click on logo Red and check URL", () => {
+    cy.get('[data-test-id="global-menu-btn"]').click();
+    cy.url().should("eq", "https://work.co/grid/");
+  });
 
-    it('Check headline',() => {
-      cy.get('[data-test-id="header-title-text"]').should('have.text','We design and ship digital products that transform companies.')
-  })
-  it('Click on logo Red and check URL', () => {
-    cy.get('[data-test-id="global-menu-btn"]').click()
-    cy.url().should('eq', 'https://work.co/grid/');
-  })
+  it("Select Clients", () => {
+    cy.get('[data-test-id="grid-select-clients-link"]').click();
+    cy.url().should("eq", "https://work.co/clients/");
+  });
 
-  it('Select Clients',()=>{
-    cy.get('[data-test-id="grid-select-clients-link"]').click()
-    cy.url().should('eq', 'https://work.co/clients/');
-  })
-  
-  it('Select Expertise and Capabilities',()=>{
-    cy.get('[data-test-id="global-menu-btn"]').click()
-    cy.get('[data-test-id="grid-practice-areas-link"]').click()
-    cy.url().should('eq', 'https://work.co/company/');
-  })
-
-  })
+  it("Select Expertise and Capabilities", () => {
+    cy.get('[data-test-id="global-menu-btn"]').click();
+    cy.get('[data-test-id="grid-practice-areas-link"]').click();
+    cy.url().should("eq", "https://work.co/company/");
+  });
+});
