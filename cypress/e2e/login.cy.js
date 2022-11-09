f1f7c957e51744835394cf2b2cdd2b9d7a66742e
kzivanovic <katarinazivanovic@BEG-Katarina-Zivanovic.local>
2022-11-09 13:09:35 +0100

    Challenge4

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,22 @@
+describe('Tes cases for Swag Labs',()=>{
+    it('Open URL check Check page title',()=>{
+        cy.visit('https://www.saucedemo.com/')
+        cy.title().should('eq','Swag Labs')
+    })
+    it('Invalid Login',()=>{
+        cy.visit('https://www.saucedemo.com')
+        cy.get('[data-test="username"]').click().type('zivanovic@work.co')
+        cy.get('[data-test="password"]').click().type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this service')
+
+    })
+    it('Valid Login',()=>{
+        cy.get('[data-test="username"]').click().clear()
+        cy.get('[data-test="password"]').click().clear()
+        cy.get('[data-test="username"]').click().type('standard_user')
+        cy.get('[data-test="password"]').click().type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        cy.url().should('contain','inventory.html')
+    })
+})
