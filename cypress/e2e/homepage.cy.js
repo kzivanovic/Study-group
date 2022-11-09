d0ea60ef026ee56381004bb0d9f1eb8e54b71378
kzivanovic <katarinazivanovic@BEG-Katarina-Zivanovic.local>
2022-11-09 16:29:05 +0100

    additional tests for work and co

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -15,10 +15,42 @@ describe('Test cases for W&Co', () => {
     cy.url().should('eq', 'https://work.co/clients/');
   })
   
-  it('Select Expertise and Capabilities',()=>{
+  it('Select Practise Areas',()=>{
     cy.get('[data-test-id="global-menu-btn"]').click()
     cy.get('[data-test-id="grid-practice-areas-link"]').click()
     cy.url().should('eq', 'https://work.co/company/');
   })
+  it('Go back to homepage',()=>{
+    cy.get('[data-test-id="global-menu-btn"]').click() 
+  })
+  it('Subscribe to Newsletter',()=>{
+    cy.get('[data-test-id="footer-subscribe-newsletter-link"]').click()
+    cy.get('[data-test-id="newsletter-subscribe-submit"]').should('be.disabled')
+    cy.get('[data-test-id="newsletter-email-textfield"]').click().type('zivanovic@work.co')
+    cy.get('[data-test-id="newsletter-subscribe-submit"]').should('be.enabled')
+
 
   })
+  it('Check Disclaimer',()=>{
+    cy.get('.Subscribe-gdprDisclaimer').should('contain','We only send emails when we have something to say.')
+    cy.get('a').contains('Privacy Policy').click()
+    cy.get('a').contains('Terms').click()
+    cy.get('[data-test-id="global-menu-btn"]').click()
+
+
+  })
+
+  it('Choose Belgarde lead position',()=>{
+    cy.get('[data-test-id="grid-careers-link"]').click()
+    cy.get('[data-test-id="grid-item-technology-link"]').click()
+    cy.get('[data-test-id="technology-grid-subitem-link-lead-developer-belgrade"]').click()
+  })
+  it('Apply for a role',()=>{
+    cy.get('[data-test-id="form-technology-belgrade-lead-developer-first_name-text"]').click().type('Katarina')
+    cy.get('[data-test-id="form-technology-belgrade-lead-developer-last_name-text"]').click().type('Barjaktarovic')
+    cy.get('[data-test-id="form-technology-belgrade-lead-developer-email-email"]').click().type('zivanovic@work.co')
+    cy.get('[data-test-id="form-technology-belgrade-lead-developer-phone-tel"]').click().type('+381691747959')
+    cy.get('[data-test-id="form-technology-belgrade-lead-developer-question_36310376-text"]').click().type('https://www.linkedin.com/in/katarina-zivanovic-aa06143a/')
+  })
+
+})
